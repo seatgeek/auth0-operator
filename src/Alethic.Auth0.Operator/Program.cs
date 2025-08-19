50e803dd64df917b0fc35dc6ae990ebfb00d4e0d
Liroy Alima (SG) <lalima@seatgeek.com>
2025-08-19 18:15:44 -0500

    In containerized environments or simple console applications, the default console formatter often doesn't include timestamps in the output format.
    Configure Microsoft.Extensions.Logging's logging formatter to include timestamps.

--- src/Alethic.Auth0.Operator/Program.cs
+++ src/Alethic.Auth0.Operator/Program.cs
@@ -7,6 +7,7 @@ using KubeOps.Operator;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Alethic.Auth0.Operator
 {
@@ -19,17 +20,22 @@ namespace Alethic.Auth0.Operator
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.AddKubernetesOperator().RegisterComponents();
             builder.Services.AddMemoryCache();
-            
+
             builder.Services.Configure<ReconciliationConfig>(config =>
             {
                 config.Interval = TimeSpan.FromSeconds(30);
-                
+
                 if (int.TryParse(Environment.GetEnvironmentVariable("RECONCILIATION_INTERVAL_SECONDS"), out var seconds))
                 {
                     config.Interval = TimeSpan.FromSeconds(seconds);
                 }
             });
-            
+
+            builder.Logging.AddSimpleConsole(options =>
+            {
+                options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+            });
+
             var app = builder.Build();
             return app.RunAsync();
         }
