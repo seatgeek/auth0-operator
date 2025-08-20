812dfa3db683c1cc02f2850852cbf9b46a8d4403
Liroy Alima (SG) <lalima@seatgeek.com>
2025-08-19 19:27:39 -0500

    Align ConnectionFind JSON property with Auth0 API naming

--- src/Alethic.Auth0.Operator.Core/Models/Connection/ConnectionFind.cs
+++ src/Alethic.Auth0.Operator.Core/Models/Connection/ConnectionFind.cs
@@ -6,7 +6,7 @@ namespace Alethic.Auth0.Operator.Core.Models.Connection
     public partial class ConnectionFind
     {
 
-        [JsonPropertyName("connectionId")]
+        [JsonPropertyName("id")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? ConnectionId { get; set; }
 
